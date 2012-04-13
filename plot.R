8174787e6db84b8497d80c5f9f84c9e2d150ee21
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-04-12 17:42:20 -0700

    do it in a loop

--- plot.R
+++ plot.R
@@ -2,42 +2,13 @@
 
 a =read.table('table', header=T);
 
-######################################################################
-sel = intersect(which(a$time_req == '5hr'), which(a$system == 'Hera'));
-
-times = a$stop_time[sel] - a$start_time[sel];
-
-pdf('hera_5hr.pdf');
-plot(a$nodes[sel], times, main=paste('Hera','time to allocation for','5hr','jobs'),ylab='time to allocation (s)', xlab='number of nodes', xaxt='n'); 
-axis(1, at=unique(a$nodes[sel]));
-dev.off();
-
-######################################################################sel = intersect(which(a$time_req == '2m'), which(a$system == 'Hera'));
-
-sel = intersect(which(a$time_req == '2m'), which(a$system == 'Hera'));
-times = a$stop_time[sel] - a$start_time[sel];
-
-pdf('hera_2m.pdf');
-plot(a$nodes[sel], times, main=paste('Hera','time to allocation for','2m','jobs'),ylab='time to allocation (s)', xlab='number of nodes', xaxt='n'); 
-axis(1, at=unique(a$nodes[sel]));
-dev.off();
-
-######################################################################
-
-sel = intersect(which(a$time_req == '5hr'), which(a$system == 'Sierra'));
-times = a$stop_time[sel] - a$start_time[sel];
-
-pdf('Sierra_5hr.pdf');
-plot(a$nodes[sel], times, main=paste('Hera','time to allocation for','5hr','jobs'),ylab='time to allocation (s)', xlab='number of nodes', xaxt='n'); 
-axis(1, at=unique(a$nodes[sel]));
-dev.off();
-
-######################################################################
-
-sel = intersect(which(a$time_req == '2m'), which(a$system == 'Sierra'));
-times = a$stop_time[sel] - a$start_time[sel];
-
-pdf('Sierra_2m.pdf');
-plot(a$nodes[sel], times, main=paste('Hera','time to allocation for','2m','jobs'),ylab='time to allocation (s)', xlab='number of nodes', xaxt='n'); 
-axis(1, at=unique(a$nodes[sel]));
-dev.off();
+for(reqTime in c('5hr', '2m')){
+  for(system in c('Hera', 'Sierra')){
+    pdf(paste(system, '_', reqTime, '.pdf', sep=''));
+    sel = intersect(which(a$time_req == reqTime), which(a$system == system));
+    times = a$stop_time[sel] - a$start_time[sel];
+    plot(a$nodes[sel], times, main=paste(system,'time to allocation for',reqTime,'jobs'),ylab='time to allocation (s)', xlab='number of nodes', xaxt='n'); 
+    axis(1, at=unique(a$nodes[sel]));
+    dev.off();
+  }
+ }
