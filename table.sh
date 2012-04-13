7ded1c77185cd78a84fa503eea8d11d4b8475356
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-04-12 17:14:54 -0700

    omit entries that failed to complete

--- table.sh
+++ table.sh
@@ -8,6 +8,8 @@ cat all | xargs grep -i error |cut -d':' -f1|uniq > auto_error
 
 cat manual_error auto_error > error
 
+grep -c stop `cat no_error `| grep ':0' >> error
+
 cat all | jtset -d error > no_error
 
 
