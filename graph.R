00e6aa57ea61f83faba95ab6c6c72683d9c085d8
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-04-22 22:29:42 -0700

    use eps, fix y axis labeling

--- graph.R
+++ graph.R
@@ -39,25 +39,26 @@ partial_offset = c(      +0.08, +0.24)
 at = c(3+offset, 4+offset, 5+offset, 6+offset, 7+offset, 8+offset, 9+partial_offset, 10+partial_offset)
 col=c( rep(c(c1, c2, c3, c4), 6), c1, c2, c1, c2)
      
-pdf(file="TimeToStart_no_outliers.pdf")
+#pdf(file="TimeToStart_no_outliers.pdf")
+postscript(file='TimeToStart_no_outliers.eps')
 boxplot( 
-	log2(c(e1, e2, e3, e4))
+	log10(c(e1, e2, e3, e4))
 	~
 	c(log2(n1)+0.01, log2(n2)+0.02, log2(n3)+0.03, log2(n4)+0.04),
 	xaxt="n",
-#	yaxt="n",
+	yaxt="n",
 	col=col,
 	outline=F,
 	at=at,
 	xlim=c(2,11),
-	ylim=c(0,16),
+	ylim=c(0,5),
 	xlab="Requested Nodes", 
-	ylab="Log2 Seconds to Job Start", main="Time to job start vs. system, requested nodes and job length",
+	ylab="Seconds to Job Start", main="Time to job start vs. system, requested nodes and job length",
 	boxwex=.13
 	)
 
 legend("bottomright", legend=c("Sierra, 2-minute job", "Sierra, 5-hour job", "Hera, 2-minute job", "Hera, 5-hour job"), fill=c(c1, c2, c3, c4) )
 
-#axis(side=2, at=seq(0,14,2), labels=10^(seq(0,14,2)-2))
+axis(side=2, at=0:5, labels=10^(0:5))
 axis(side=1, at=3:10, labels=2^(3:10))
 dev.off()
